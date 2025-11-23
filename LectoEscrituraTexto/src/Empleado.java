e6f6b516f8b7d90c9a8f820144da111dc3944d7c
Manu <dominguez.dieguez.manuel.jesus@iescamas.es>
2025-11-23 19:17:15 +0100

    LEctoEscritura de archivos de texto

---
+++ LectoEscrituraTexto/src/Empleado.java
@@ -0,0 +1,45 @@
+public class Empleado {
+
+    protected String nombre; 
+    protected String apellido; 
+    protected int edad; 
+
+    public Empleado(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " nombre='" + getNombre() + "'" +
+            ", apellido='" + getApellido() + "'" +
+            ", edad='" + getEdad() + "'" +
+            "}";
+    }
+}
