cfcfd06d7a660914c4efeaa65db9bfc1801fcd50
unknown <dominguez.dieguez.manuel.jesus@iescamas.es>
2025-11-26 19:40:28 +0100

    Minor changes

--- EjerciciosTema1/EjercicioDOM/src/App.java
+++ EjerciciosTema1/EjercicioDOM/src/App.java
@@ -11,7 +11,6 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class App {
-
     public static void main(String[] args) {
         try {
             File file = new File("libros.xml"); 
